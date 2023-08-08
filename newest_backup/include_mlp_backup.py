e944374c4c73fb03def4d153307e85c35ee17ca0
Cody Rushing <thisiscodyr@yahoo.com>
2023-08-08 17:09:49 +0000

    Imports

---
+++ newest_backup/include_mlp_backup.py
@@ -0,0 +1,12 @@
+# %%
+!pip install git+https://github.com/neelnanda-io/TransformerLens.git
+!sudo apt install unzip
+!pip install git+https://github.com/callummcdougall/CircuitsVis.git#subdirectory=python
+!pip install git+https://github.com/neelnanda-io/neel-plotly.git
+!pip install plotly
+
+from imports import *
+
+# %%
+
+
