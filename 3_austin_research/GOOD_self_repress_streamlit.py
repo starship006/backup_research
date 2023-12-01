f1ced931bcfb2aa71febba2460d1d75473ae7452
codyr <thisiscodyr@yahoo.com>
2023-12-01 00:27:02 -0600

    update streamlit code

--- 3_austin_research/GOOD_self_repress_streamlit.py
+++ 3_austin_research/GOOD_self_repress_streamlit.py
@@ -30,7 +30,7 @@ def plot_results(original_de, new_de, receive_heads, scaling, output_type, recei
         yaxis_title="New Direct Effect"
     )
 
-    fig.show()
+    return fig
     
 # %%
     
@@ -38,16 +38,20 @@ def load_results(model_name):
     with open(f'results_{model_name}.pickle', 'rb') as f:
         return pickle.load(f)
 
-model_name = st.sidebar.selectbox("Select model", ["pythia-160m", "gpt2-small"])
+model_name = st.sidebar.selectbox("Select model", ["pythia-160m", "gpt2-small", "gpt-neo-125m", "gpt2-large",
+                                                   "gpt2-medium", "pythia-410m", "stanford-gpt2-medium-a", "stanford-gpt2-small-a"])
 results = load_results(model_name) 
 
-output_type = st.sidebar.selectbox("Output type", options=[r[0] for r in results.keys()])
-receiving_type = st.sidebar.selectbox("Receiving type", options=[r[1] for r in results.keys()]) 
-scaling = st.sidebar.selectbox("Scaling", options=[r[2] for r in results.keys()])
+output_type = st.sidebar.selectbox("Output type", options={r[0] for r in results.keys()})
+receiving_type = st.sidebar.selectbox("Receiving type", options={r[1] for r in results.keys()}) 
+scaling = st.sidebar.selectbox("Scaling", options={r[2] for r in results.keys()})
 
 orig_de, new_de, heads = results[(output_type, receiving_type, scaling)]
 
 st.subheader(f"Results for {output_type}, {receiving_type}, scaling={scaling}")
-plot_results(orig_de, new_de, heads, scaling, output_type, receiving_type)
+fig = plot_results(orig_de, new_de, heads, scaling, output_type, receiving_type)
+
+st.plotly_chart(fig)
+
 
 # %%
