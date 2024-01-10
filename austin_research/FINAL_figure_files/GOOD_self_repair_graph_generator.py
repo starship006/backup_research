98b1be587e7d0bcce2b8c30f80e91df5cdf444f3
Cody <thisiscodyr@yahoo.com>
2024-01-10 23:08:43 +0000

    Support for Llama in graph generation + formatting

--- austin_research/FINAL_figure_files/GOOD_self_repair_graph_generator.py
+++ austin_research/FINAL_figure_files/GOOD_self_repair_graph_generator.py
@@ -4,18 +4,18 @@ This code is responsible for making the self-repair graphs.
 # %%
 from imports import *
 from path_patching import act_patch
-from GOOD_helpers import is_notebook, shuffle_owt_tokens_by_batch, return_item, get_correct_logit_score, collect_direct_effect, show_input, create_layered_scatter, replace_output_hook
+from GOOD_helpers import is_notebook, shuffle_owt_tokens_by_batch, return_item, get_correct_logit_score, collect_direct_effect, create_layered_scatter, replace_output_hook
 # %% Constants
 in_notebook_mode = is_notebook()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-FOLDER_TO_WRITE_GRAPHS_TO = "figures/self_repair_graphs/"
-FOLDER_TO_STORE_PICKLES = "pickle_storage/"
-
+FOLDER_TO_WRITE_GRAPHS_TO = "figures/new_self_repair_graphs/"
+FOLDER_TO_STORE_PICKLES = "pickle_storage/new_graph_pickle/"
+NO_PADDING = True
 
 if in_notebook_mode:
-    model_name = "pythia-160m"
-    BATCH_SIZE = 30
-    ABLATION_TYPE = "mean" 
+    model_name = "gpt2-medium"#"pythia-160m"####
+    BATCH_SIZE = 2
+    ABLATION_TYPE = "sample" 
 else:
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='gpt2-small')
@@ -30,24 +30,46 @@ else:
 # Ensure that ABLATION_TYPE is one of the expected values
 assert ABLATION_TYPE in ["mean", "zero", "sample"], "Ablation type must be 'mean', 'zero', or 'sample'."
 
-# %%
+# %% Import the Model
+from transformers import LlamaForCausalLM, LlamaTokenizer
+from constants import LLAMA_MODEL_PATH # change LLAMA_MODEL_PATH to the path of your llama model weights
+
+if "llama" in model_name:
+    tokenizer = LlamaTokenizer.from_pretrained(LLAMA_MODEL_PATH) 
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '<unk>'})
+    
+    hf_model = LlamaForCausalLM.from_pretrained(LLAMA_MODEL_PATH, low_cpu_mem_usage=True)
+    
+    model = HookedTransformer.from_pretrained("llama-7b", hf_model=hf_model, device="cpu", fold_ln=False, center_writing_weights=False, center_unembed=False, tokenizer=tokenizer)
+    model: HookedTransformer = model.to("cuda" if torch.cuda.is_available() else "cpu") #type: ignore
+else:
+    model = HookedTransformer.from_pretrained(
+        model_name,
+        center_unembed = True,
+        center_writing_weights = True,
+        fold_ln = True, # TODO; understand this
+        refactor_factored_attn_matrices = False,
+        device = device,
+    )
+
 safe_model_name = model_name.replace("/", "_")
-model = HookedTransformer.from_pretrained(
-    model_name,
-    center_unembed = True, 
-    center_writing_weights = True,
-    fold_ln = True, # TODO; understand this
-    refactor_factored_attn_matrices = False,
-    device = device,
-)
 model.set_use_attn_result(False)
+
 
 # %%
 dataset = utils.get_dataset("pile")
 dataset_name = "The Pile"
-all_dataset_tokens = model.to_tokens(dataset["text"]).to(device)
+
+PROMPT_LEN = 200
+
+if NO_PADDING:
+    new_dataset = utils.tokenize_and_concatenate(dataset, model.tokenizer, max_length = PROMPT_LEN) #type: ignore
+    all_dataset_tokens = new_dataset['tokens'].to(device) #type: ignore
+else:
+    all_dataset_tokens = model.to_tokens(dataset["text"]).to(device)
 # %% Helper Functions
-def new_logits_upon_ablation_calc(clean_tokens, corrupted_tokens, heads = None, mlp_layers = None, num_runs = 1, clean_cache: ActivationCache = None):
+def new_logits_upon_ablation_calc(clean_tokens, corrupted_tokens, logits: Tensor, heads = None, mlp_layers = None, num_runs = 1, clean_cache: Union[None, ActivationCache] = None):
     """
     gets the logits of correct token when running the model on some tokens and ablating some heads or mlp layers
     averaged over num_runs different ablations (by default, only just 1 run. this may need to be greater if working with smaller batch sizes)
@@ -63,13 +85,13 @@ def new_logits_upon_ablation_calc(clean_tokens, corrupted_tokens, heads = None, 
     if ABLATION_TYPE == "zero" or ABLATION_TYPE == "mean":
         num_runs = 1 # since zero or mean ablation would be deterministic
     
-    logits_accumulator = torch.zeros_like(logits)
-    for i in range(num_runs):
+    logits_accumulator = torch.zeros_like(logits, device=logits.device)
+    for _ in range(num_runs):
         # Shuffle owt_tokens by batch
         shuffled_corrupted_tokens = shuffle_owt_tokens_by_batch(corrupted_tokens)
         # Calculate new_logits using act_patch
         if ABLATION_TYPE == "zero":
-            new_logits = act_patch(model, clean_tokens, nodes, return_item, new_cache = "zero", apply_metric_to_cache=False)
+            new_logits = act_patch(model, clean_tokens, nodes, return_item, new_cache = "zero", apply_metric_to_cache=False)# type: ignore
         elif ABLATION_TYPE == "mean":
             assert clean_cache is not None, "clean_cache must be provided for mean ablation"
             assert len(nodes) == 1 and mlp_layers == None and heads != None, "mean ablation currently only works for one head"
@@ -96,7 +118,7 @@ def new_logits_upon_ablation_calc(clean_tokens, corrupted_tokens, heads = None, 
     avg_correct_logit_score = get_correct_logit_score(avg_logits, clean_tokens)
     return avg_correct_logit_score
 
-def get_thresholded_change_in_logits(clean_tokens, corrupted_tokens, per_component_direct_effect: Tensor, heads = None, mlp_layers = None, thresholds = [0.5], cache: ActivationCache = None):
+def get_thresholded_change_in_logits(clean_tokens, corrupted_tokens, logits: Tensor, per_component_direct_effect: Tensor, heads = None, mlp_layers = None, thresholds = [0.5], cache: Union[None, ActivationCache] = None):
     """"
     this function calculates direct effect of component on clean runs and ablations,
     and then returns an averaged direct effect and compensatory response effect of the component
@@ -121,16 +143,16 @@ def get_thresholded_change_in_logits(clean_tokens, corrupted_tokens, per_compone
         layer = max(mlp_layers)
         head = None
     else:
-        raise Exception("No heads or mlp layers given")
+        raise ValueError("No heads or mlp layers given")
     
     nodes = []
-    if heads != None :
+    if heads is not None :
         nodes += [Node("z", layer, head) for (layer,head) in heads]
-    if mlp_layers != None:
+    if mlp_layers is not None:
         nodes += [Node("mlp_out", layer) for layer in mlp_layers]
 
     
-    change_in_logits = new_logits_upon_ablation_calc(clean_tokens, corrupted_tokens, heads, mlp_layers, clean_cache=cache) - get_correct_logit_score(logits, clean_tokens)
+    change_in_logits = new_logits_upon_ablation_calc(clean_tokens, corrupted_tokens, logits, heads, mlp_layers, clean_cache=cache) - get_correct_logit_score(logits, clean_tokens)
         
     # 3) filter for indices wherer per_component_direct_effect is greater than threshold
     to_return = {}
@@ -154,16 +176,15 @@ def get_thresholded_change_in_logits(clean_tokens, corrupted_tokens, per_compone
         to_return[f"num_thresholded_{threshold}"] = selected_cil.shape[0]
     return to_return
 
-
-
 # %% We need to iterate through the dataset
-TOTAL_PROMPTS_TO_ITERATE_THROUGH = 3000
+TOTAL_PROMPTS_TO_ITERATE_THROUGH = 240 #around 500 / 2
 
 # BATCH_SIZE defined earlier
-PROMPT_LEN = 400
 
 num_batches = TOTAL_PROMPTS_TO_ITERATE_THROUGH // BATCH_SIZE
-
+assert model.tokenizer is not None
+PAD_TOKEN = model.to_tokens(model.tokenizer.pad_token)[-1, -1].item() 
+print("Percent of tokens that are padding tokens: ", (all_dataset_tokens[:(TOTAL_PROMPTS_TO_ITERATE_THROUGH + BATCH_SIZE), :PROMPT_LEN] == PAD_TOKEN).flatten().float().mean(-1))
 # %% We filter out for specific instances where the direct effects are above a certain amount. By default, the figures in the paper don't worry about thresholds, but they are nice to have
 THRESHOLDS = [0.0] #[0.1 * i for i in range(0,12)]
 thresholded_de = torch.zeros((num_batches, len(THRESHOLDS), model.cfg.n_layers, model.cfg.n_heads))
@@ -186,11 +207,12 @@ for batch in tqdm(range(num_batches)):
     assert clean_tokens.shape == corrupted_tokens.shape == (BATCH_SIZE, PROMPT_LEN)
     
     
+    
     # Cache clean/corrupted model activations + direct effects
     logits, cache = model.run_with_cache(clean_tokens)
-    corrupted_logits, corrupted_cache = model.run_with_cache(corrupted_tokens)
+    assert isinstance(logits, Tensor)
     
-
+    #corrupted_logits, corrupted_cache = model.run_with_cache(corrupted_tokens)
     per_head_direct_effect, all_layer_direct_effect = collect_direct_effect(cache, correct_tokens=clean_tokens, model = model, display = False, collect_individual_neurons = False)
     
     #if in_notebook_mode:
@@ -198,7 +220,7 @@ for batch in tqdm(range(num_batches)):
     
     for layer in range(model.cfg.n_layers):
         for head in range(model.cfg.n_heads):
-            dict_results = get_thresholded_change_in_logits(clean_tokens, corrupted_tokens, per_head_direct_effect, heads = [(layer, head)], thresholds = THRESHOLDS, cache = cache)
+            dict_results = get_thresholded_change_in_logits(clean_tokens, corrupted_tokens, logits, per_head_direct_effect, heads = [(layer, head)], thresholds = THRESHOLDS, cache = cache)
             for i, threshold in enumerate(THRESHOLDS):
                 thresholded_de[batch, i, layer, head] = dict_results[f"de_{threshold}"]
                 thresholded_cil[batch, i, layer, head] = dict_results[f"cil_{threshold}"]
